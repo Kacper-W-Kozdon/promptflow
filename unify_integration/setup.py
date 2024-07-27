dfe83c2575334cd58881049657a8f3d136138161
Kacper-W-Kozdon <k.kozdon@gmail.com>
2024-07-27 14:26:21 +0200

    renames unify_promptflow to unify_integration and other name changes

--- unify_integration/setup.py
+++ unify_integration/setup.py
@@ -1,6 +1,6 @@
 from setuptools import find_packages, setup
 
-PACKAGE_NAME = "unify_promptflow"
+PACKAGE_NAME = "unify_integration"
 
 setup(
     name=PACKAGE_NAME,
@@ -8,7 +8,7 @@ setup(
     description="This is my tools package",
     packages=find_packages(),
     entry_points={
-        "package_tools": ["optimize_llm_tool = unify_promptflow.tools.utils:list_package_tools"],
+        "package_tools": ["optimize_llm_tool = unify_integration.tools.utils:list_package_tools"],
     },
     include_package_data=True,   # This line tells setuptools to include files from MANIFEST.in
 )
