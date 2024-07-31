d973e59479b2cb63c72975c101ba8383a5e08fcf
Kacper-W-Kozdon <k.kozdon@gmail.com>
2024-07-31 13:27:09 +0200

    adds optimize_llm function body

--- unify_integration/tests/test_unify_llm_tool.py
+++ unify_integration/tests/test_unify_llm_tool.py
@@ -2,24 +2,25 @@ import pytest
 import unittest
 
 from promptflow.connections import CustomConnection
-from unify_promptflow.tools.unify_tool import optimize_llm
+from unify_integration.unify_llm_tool.tools.optimize_llm_tool import optimize_llm
+from unify import Unify
+from dotenv import load_dotenv
+import os
 
+load_dotenv()
+unify_api_key = os.getenv("UNIFY_KEY")
 
 @pytest.fixture
-def my_custom_connection() -> CustomConnection:
-    my_custom_connection = CustomConnection(
-        {
-            "api-key" : "my-api-key",
-            "api-secret" : "my-api-secret",
-            "api-url" : "my-api-url"
-        }
+def my_custom_connection() -> Unify:
+    my_custom_connection = Unify(
+        api_key=unify_api_key
     )
     return my_custom_connection
 
 
 class TestTool:
-    def test_optimize_llm(self, my_custom_connection):
-        result = optimize_llm(my_custom_connection, input_text="Microsoft")
+    def test_optimize_llm(self, my_custom_connection, config):
+        result = optimize_llm(my_custom_connection, config, input_text="Microsoft")
         assert result == "Hello Microsoft"
 
 
--- unify_integration/unify_llm_tool/tools/optimize_llm_tool.py
+++ unify_integration/unify_llm_tool/tools/optimize_llm_tool.py
@@ -1,14 +1,24 @@
 from promptflow.core import tool
 from promptflow.connections import CustomConnection
-from typing import Union, Optional
+from typing import Union, Optional, Sequence
+from unify import Unify
 
 
 @tool
-def optimize_llm(connection: CustomConnection, config: dict) -> Union[dict, str]:
-"""
-Selects the optimal model for a step of a flow.
+def optimize_llm(connection: Unify, config: dict, input_text: Union[str, Sequence]) -> Union[dict, str]:
+    """
+    Selects the optimal model for a step of a flow.
 
-:param connection: CustomConnection to use
-:param config: requirements for the optimization
-"""
+    :param connection: CustomConnection to use
+    :param config: requirements for the optimization
+    """
+    endpoint = config.get("endpoint", None)
+    model = config.get("model", None)
+    provider = config.get("provider", None)
+    if endpoint:
+        connection.set_endpoint(endpoint)
+    if not endpoint and all([model, provider]):
+        connection.set_model(model)
+        connection.set_provider(provider)
+
     return "Hello " + input_text
